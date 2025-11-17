7d86b94c7e7bd6eba48c877c21d9d26f8303a7c8
joaopedped-code <joaopedroevangelistalopes@gmail.com>
2025-11-17 18:34:47 -0300

    Atualiza main.lua para PushBlind

--- main.lua
+++ main.lua
@@ -1,10 +1,41 @@
-return {
-    install = function(ctx)
-        print("Nada para instalar (install vazio).")
-    end,
+local M = {}
 
-    pack = function(ctx)
-        ctx:sh("zip -j hello_package.zip hello.py")
-        print("Pacote criado: hello_package.zip")
-    end
-}
+-- INSTALAR
+function M.install(ctx)
+    print("Instalando hello_python...")
+
+    os.execute('curl -L https://raw.githubusercontent.com/mateusmoutinho/hello_world_python/main/hello.py -o hello_python')
+
+    os.execute("sudo mv hello_python /usr/bin/hello_python")
+    os.execute("sudo chmod +x /usr/bin/hello_python")
+
+    print("hello_python instalado!")
+end
+
+-- ATUALIZAR
+function M.update(ctx)
+    print("Atualizando hello_python...")
+
+    os.execute('curl -L https://raw.githubusercontent.com/mateusmoutinho/hello_world_python/main/hello.py -o hello_python')
+
+    os.execute("sudo mv hello_python /usr/bin/hello_python")
+    os.execute("sudo chmod +x /usr/bin/hello_python")
+
+    print("hello_python atualizado!")
+end
+
+-- REMOVER
+function M.remove(ctx)
+    print("Removendo hello_python...")
+
+    os.execute("sudo rm -f /usr/bin/hello_python")
+
+    print("hello_python removido!")
+end
+
+-- Comando customizado
+function M.run(ctx)
+    os.execute("python3 /usr/bin/hello_python")
+end
+
+return M
