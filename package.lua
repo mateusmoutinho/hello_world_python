720f7eeb56741327a62290031fadd52f4266cda8
joaopedped-code <joaopedroevangelistalopes@gmail.com>
2025-11-17 18:45:32 -0300

    Fix PushBlind package format

--- package.lua
+++ package.lua
@@ -1,19 +1,28 @@
-return {
-    name = "hello_python",
+local package = {}
 
-    install = function(self)
-        print("[hello_python] Instalando...")
-        local src = self:path("hello.py")
-        local dst = self:bin_path("hello_python")
+package.name = "hello_python"
 
-        os.execute("cp " .. src .. " " .. dst)
-        os.execute("chmod +x " .. dst)
+function package.install(self)
+    print("[hello_python] Instalando...")
 
-        print("[hello_python] Instalado!")
-    end,
+    local src = self:path("hello.py")
+    local dst = self:bin_path("hello_python")
 
-    run = function(self, args)
-        local exec = self:bin_path("hello_python")
-        os.execute(exec .. " " .. (args or ""))
-    end
-}
+    os.execute("cp " .. src .. " " .. dst)
+    os.execute("chmod +x " .. dst)
+
+    print("[hello_python] Instalado!")
+end
+
+function package.run(self, args)
+    local exec = self:bin_path("hello_python")
+    os.execute(exec .. " " .. (args or ""))
+end
+
+function package.remove(self)
+    local dst = self:bin_path("hello_python")
+    os.execute("rm -f " .. dst)
+    print("[hello_python] Removido!")
+end
+
+return package
