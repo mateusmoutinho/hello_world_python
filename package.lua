3d88bc78c66512306201b5f4de7daea3fe4f5c31
joaopedped-code <joaopedroevangelistalopes@gmail.com>
2025-11-17 18:41:05 -0300

    Add install/run actions

--- package.lua
+++ package.lua
@@ -1,51 +1,19 @@
-local PushBlind = {}
-
--- Metadados do pacote
-PushBlind.meta = {
+return {
     name = "hello_python",
-    version = "1.0.0",
-    description = "Instala o script hello.py como comando hello_python"
+
+    install = function(self)
+        print("[hello_python] Instalando...")
+        local src = self:path("hello.py")
+        local dst = self:bin_path("hello_python")
+
+        os.execute("cp " .. src .. " " .. dst)
+        os.execute("chmod +x " .. dst)
+
+        print("[hello_python] Instalado!")
+    end,
+
+    run = function(self, args)
+        local exec = self:bin_path("hello_python")
+        os.execute(exec .. " " .. (args or ""))
+    end
 }
-
--- Ações
-PushBlind.actions = {}
-
--- INSTALAR
-function PushBlind.actions.install(ctx)
-    print("Instalando hello_python...")
-
-    os.execute('curl -L https://raw.githubusercontent.com/mateusmoutinho/hello_world_python/main/hello.py -o hello_python')
-
-    os.execute("sudo mv hello_python /usr/bin/hello_python")
-    os.execute("sudo chmod +x /usr/bin/hello_python")
-
-    print("hello_python instalado!")
-end
-
--- ATUALIZAR
-function PushBlind.actions.update(ctx)
-    print("Atualizando hello_python...")
-
-    os.execute('curl -L https://raw.githubusercontent.com/mateusmoutinho/hello_world_python/main/hello.py -o hello_python')
-
-    os.execute("sudo mv hello_python /usr/bin/hello_python")
-    os.execute("sudo chmod +x /usr/bin/hello_python")
-
-    print("hello_python atualizado!")
-end
-
--- REMOVER
-function PushBlind.actions.remove(ctx)
-    print("Removendo hello_python...")
-
-    os.execute("sudo rm -f /usr/bin/hello_python")
-
-    print("hello_python removido!")
-end
-
--- RODAR
-function PushBlind.actions.run(ctx)
-    os.execute("python3 /usr/bin/hello_python")
-end
-
-return PushBlind
